7504847e8d0246c60cfaae16b8d81386950fcc40
hayeong120 <jbg01200249@gmail.com>
2023-10-26 10:00:00 +0900

    0528 수업코드 - 부모/ 자식 생성자, 소멸자 호출순서

--- 05-01 상속기본.cpp
+++ 05-01 상속기본.cpp
@@ -4,8 +4,12 @@ using namespace std;
 
 class Person {
 public:
-	Person(string name, int age)
-		: name_(name), age_(age) {}
+	Person(string name, int age) : name_(name), age_(age) {
+		cout << "부모 생성자 호출" << endl;
+	}
+	~Person(){ 
+		cout << "부모 소멸자 호출" << endl;
+	}
 
 	void show_Person() {
 		cout << name_ << "의 나이는 " << age_ << "입니다." << endl;
@@ -20,11 +24,15 @@ class Student : public Person {
 public:
 	// 부모 생성자가 먼저 호출
 	Student(string name, int age, int id) : Person(name, age), id_(id) {
-
+		cout << "자식 생성자 호출" << endl;
 	}
+	~Student() {
+		cout << "자식 소멸자 호출" << endl;
+	}
+
 	void show_Person() {
+		cout << id_ << " ";
 		Person::show_Person();	// 부모 클래스의 멤버함수 호출
-		cout << id_ << endl;
 	}
 
 private:
@@ -34,6 +42,8 @@ private:
 int main(void) {
 	Student* stu = new Student("하영0l", 18, 2211);
 	stu->show_Person();
+
+	delete stu;
 
 	return 0;
 }
