df84f679f9bf0535606669e041e7e8adec937cf8
hayeong120 <jbg01200249@gmail.com>
2024-03-26 15:42:03 +0900

    0326 수업코드 - call by pointer

--- 2024-CPP-Study/2024-CPP-Study/call_by.cpp
+++ 2024-CPP-Study/2024-CPP-Study/call_by.cpp
@@ -3,16 +3,16 @@
 // 이름공간. std::를 안해도 된다.
 using namespace std;
 
-void call_by_value(int val) 
+void call_by_pointer(int* val) 
 {
-	// 새로운 지역변수 val을 만들고 거기에 10을 넣는다. 
-	val = 10;
+	// 새로운 지역변수 val을 만들고 val이 가리키고 있는 대상의 값을 바꾼다. 
+	*val = 10;
 }
 
 int main() {
 	int num = 4;
 	// num의 값은 바뀌지 않는다. 
-	call_by_value(num);
+	call_by_pointer(&num);
 	cout << num;
 
 	return 0;
