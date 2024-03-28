37706b728196c8f2b12e7d4cec86c1654bb348c0
hayeong120 <jbg01200249@gmail.com>
2024-03-28 11:18:51 +0900

    0328 수업코드 - 포인터가 배열을 가리키는 원리

--- 2024-CPP-Study/2024-CPP-Study/배열과 포인터.cpp
+++ 2024-CPP-Study/2024-CPP-Study/배열과 포인터.cpp
@@ -3,11 +3,30 @@
 int main() {
 	int arr[4] = { 1, 2, 3, 4 };
 	// 주소값 4바이트 만큼의 차이
-	printf("%d %d\n", &arr[0], &arr[1]);
+	printf("%d %d\n", arr[0], arr[1]);
+	printf("%d %d\n", *(arr + 0), *(arr + 1));
 
 	char str[4] = "abc";
 	// 주소값 1바이트 만큼의 차이
 	printf("%d %d\n", &str[0], &str[1]);
+
+
+	int i = 1;
+	char c = 'b';
+	double d = 3.0;
+
+	int* pi = &i;
+	char* pc = &c;
+	double* pd = &d;
+
+	// 각각 4, 1, 8 차이
+	printf("%d %d %d \n", pi, pc, pd);
+	printf("%d %d %d \n", pi+1, pc+1, pd+1);
+
+	pi = arr;
+	printf("%d %d\n", pi + 1, arr + 1);
+	printf("%d %d\n", *(pi + 1), *(arr + 1));
+	printf("%d %d\n", pi[1], arr[1]);
 
 	return 0;
 }
