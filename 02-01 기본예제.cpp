0c81de236e5b3e835e7d068506d7d59e2a717517
hayeong120 <jbg01200249@gmail.com>
2024-04-25 23:18:05 +0900

    02-01 기본예제 : cout 출력객체, endl 개행객체

---
+++ 02-01 기본예제.cpp
@@ -0,0 +1,9 @@
+#include <iostream>		// 표준입출력 라이브러리 C언어의 stdio.h
+
+using namespace std;	// 이름공간 축약 std::cout -> cout
+
+int main(void)
+{
+	cout << "HELLO WORLD" << endl;		// cout: 출력객체, endl: 개행객체
+	return 0;
+}
